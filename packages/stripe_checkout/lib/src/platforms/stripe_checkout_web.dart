1cfd6b914fc40c7f15652857ad3afc24f26a4361
Maurizio Pinotti <maurizio.pinotti@euris.it>
2022-09-21 11:39:14 +0200

    Fix for Stripe web not compiling
    After 478fe92 (support for connected accounts) the Web integration no
    longer compiles due to missing stripeAccountId parameter. This commit
    just fixes the compilation error but does not implement the actual
    functionality for connected accounts on Web.

--- packages/stripe_checkout/lib/src/platforms/stripe_checkout_web.dart
+++ packages/stripe_checkout/lib/src/platforms/stripe_checkout_web.dart
@@ -6,6 +6,7 @@ Future<CheckoutResponse> redirectToCheckout({
   required BuildContext context,
   required String sessionId,
   required String publishableKey,
+  String? stripeAccountId,
   String? successUrl,
   String? canceledUrl,
 }) async {
